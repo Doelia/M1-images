c92de7f8cbf1b9f35782d832ef7b2657d50da219
doelia <kyxalia@gmail.com>
2015-02-06 12:35:13 +0100

    ajout de la normalisation

--- tp3/histrogramme.cpp
+++ tp3/histrogramme.cpp
@@ -4,6 +4,76 @@
 
 using namespace std;
 
+float* normaliser(ImageBase &im, int* vals) {
+	float size = im.getHeight() * im.getWidth();
+	float* out = (float*) malloc(256*sizeof(float));
+
+	for (int i = 0; i < 256; ++i) {
+		out[i] = (float) vals[i] / size;
+	}
+	return out;
+}
+
+float* repartition(float* vals) {
+	float sum = 0;
+	float* out = (float*) malloc(256*sizeof(float));
+	for (int i = 0; i < 256; ++i) {
+		sum += vals[i];
+		out[i] = sum;
+	}
+	return out;
+}
+
+int** histogramme(ImageBase &imIn) {
+
+	int decal = imIn.getColor()?3:1;
+
+	int** colors = (int**) malloc(3*sizeof(int));
+	for (int c = 0; c < decal; c++) {
+		colors[c] = (int*) malloc(256*sizeof(int));
+	}
+
+	for (int c = 0; c < 3; ++c)
+	for (int i = 0; i < 256; ++i) {
+		colors[c][i] = 0;
+	}
+	
+	for (int x = 0; x < imIn.getHeight(); ++x) {
+		for (int y = 0; y < imIn.getWidth(); ++y) {
+			for (int c = 0; c < decal; c++) {
+				int value = imIn[x*decal][y*decal+c];
+				colors[c][value]++;
+			}
+		}
+	}
+
+	return colors;
+}
+
+float** repartitionNormalize(ImageBase &im, int** histo) {
+	float** out = (float**) malloc(3*sizeof(float));
+	for (int c = 0; c < 3; c++) {
+		out[c] = (float*) malloc(256*sizeof(float));
+	}
+
+	for (int i = 0; i < 3; ++i) {
+		out[i] = normaliser(im, histo[i]);
+		out[i] = repartition(out[i]);
+	}
+
+	return out;
+}
+
+/*
+int integrale(int vals[], int a, int b) {
+	int sum = 0;
+	for (int i = a; i < b; ++i) {
+		sum += vals[i];
+	}
+	return sum;
+}
+*/
+
 int main(int argc, char **argv)
 {
 	//// Parametres
@@ -18,28 +88,21 @@ int main(int argc, char **argv)
 	ImageBase imIn;
 	imIn.load(input);
 
-	int colors[3][256];
-
-	for (int c = 0; c < 3; ++c)
-	for (int i = 0; i < 256; ++i) {
-		colors[c][i] = 0;
-	}
-
-	int decal = imIn.getColor()?3:1;
-	
-	cout << imIn.getWidth() << "*" << imIn.getHeight() << endl;
-	for (int x = 0; x < imIn.getHeight(); ++x) {
-		for (int y = 0; y < imIn.getWidth(); ++y) {
-			for (int c = 0; c < decal; c++) {
-				int value = imIn[x*decal][y*decal+c];
-				colors[c][value]++;
-			}
-		}
-	}
+	int** colors = histogramme(imIn);
 
 	for (int i = 0; i < 256; ++i) {
-		printf("%d %d %d %d\n", i, colors[0][i], colors[1][i], colors[2][i]);
+		//printf("%d %d %d %d\n", i, colors[0][i], colors[1][i], colors[2][i]);
 	}
+
+	float** integrs = repartitionNormalize(imIn, colors);
+
+	for (int i = 0; i < 256; ++i) {
+		printf("%d %f %f %f\n", i, integrs[0][i], integrs[1][i], integrs[2][i]);
+	}
+
+	// plot 'repart.txt' using 1:2 with lines, 'repart.txt' using 1:3 with lines, 'repart.txt' using 1:4 with lines
+	// plot 'histo.txt' using 1:2 with lines, 'histo.txt' using 1:3 with lines, 'histo.txt' using 1:4 with lines
 
 	return 0;
 }
+
--- tp3/transform.cpp
+++ tp3/transform.cpp
@@ -11,10 +11,68 @@ float transform(int val, int a, int b) {
 		val = b;
 	int ecart = b - a ;
 	float produit = (val-a) * 255.0 / ecart;
-	//cout << val << " > " << produit + a << endl;
 	return produit;
 }
 
+float* normaliser(ImageBase &im, int* vals) {
+	float size = im.getHeight() * im.getWidth();
+	float* out = (float*) malloc(256*sizeof(float));
+
+	for (int i = 0; i < 256; ++i) {
+		out[i] = (float) vals[i] / size;
+	}
+	return out;
+}
+
+float* repartition(float* vals) {
+	float sum = 0;
+	float* out = (float*) malloc(256*sizeof(float));
+	for (int i = 0; i < 256; ++i) {
+		sum += vals[i];
+		out[i] = sum;
+	}
+	return out;
+}
+
+int** histogramme(ImageBase &imIn) {
+
+	int decal = imIn.getColor()?3:1;
+
+	int** colors = (int**) malloc(3*sizeof(int));
+	for (int c = 0; c < decal; c++) {
+		colors[c] = (int*) malloc(256*sizeof(int));
+	}
+
+	for (int c = 0; c < 3; ++c)
+	for (int i = 0; i < 256; ++i) {
+		colors[c][i] = 0;
+	}
+	
+	for (int x = 0; x < imIn.getHeight(); ++x) {
+		for (int y = 0; y < imIn.getWidth(); ++y) {
+			for (int c = 0; c < decal; c++) {
+				int value = imIn[x*decal][y*decal+c];
+				colors[c][value]++;
+			}
+		}
+	}
+
+	return colors;
+}
+
+float** repartitionNormalize(ImageBase &im, int** histo) {
+	float** out = (float**) malloc(3*sizeof(float));
+	for (int c = 0; c < 3; c++) {
+		out[c] = (float*) malloc(256*sizeof(float));
+	}
+
+	for (int i = 0; i < 3; ++i) {
+		out[i] = normaliser(im, histo[i]);
+		out[i] = repartition(out[i]);
+	}
+
+	return out;
+}
 int main(int argc, char **argv)
 {
 	//// Parametres
@@ -32,6 +90,7 @@ int main(int argc, char **argv)
 	ImageBase imIn;
 	imIn.load(input);
 
+	/*
 	int min[3] = {255, 255, 255};
 	int max[3] = {0, 0, 0};
 	int decal = imIn.getColor()?3:1;
@@ -49,11 +108,11 @@ int main(int argc, char **argv)
 			}
 		}
 	}
-	/*
+	*/
+
 	int min[3] = {20, 20, 20};
 	int max[3] = {100, 100, 100};
 	int decal = imIn.getColor()?3:1;
-	*/
 
 	cout << "alpha(r) = " << min[0] << endl;
 	cout << "alpha(g) = " << min[1] << endl;
@@ -64,11 +123,14 @@ int main(int argc, char **argv)
 	cout << "beta(b) = " << max[2] << endl;
 
 	ImageBase imG(imIn.getWidth(), imIn.getHeight(), imIn.getColor());
+
+	float** normalFct = repartitionNormalize(imIn, histogramme(imIn));
 	
 	for (int x = 0; x < imIn.getHeight(); ++x) {
 		for (int y = 0; y < imIn.getWidth(); ++y) {
 			for (int c = 0; c < decal; c++) {
-				imG[x*decal][y*decal+c] = transform(imIn[x*decal][y*decal+c], min[c], max[c]);
+				int val = imIn[x*decal][y*decal+c];
+				imG[x*decal][y*decal+c] = normalFct[c][val]*val;
 			}
 		}
 	}
