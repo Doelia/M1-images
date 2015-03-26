07cb9f2ae36baf43a7fbd3ac3462da62463172fb
Doelia <kyxalia@gmail.com>
2015-03-26 11:43:58 +0100

    ajout TP2

--- subsol/TP2/main.cpp
+++ subsol/TP2/main.cpp
@@ -6,18 +6,19 @@
 
 using namespace std;
 
-
-int main() {
-	char whatisit[] = "../ressources/WHATISIT/whatisit.301x324x56.1.1.1.4.img";
-	char engine[] = "../ressources/engine/engine.256x256x128.1x1x1.img";
-	Image in(256, 256, 128);
-	//Image in(301, 324, 56);
-	in.load(engine);
-
+void printModel(const char* path, int seuil, int sizeX, int sizeY, int sizeZ) {
+	Image in(sizeX, sizeY, sizeZ);
+	in.load(path);
 	cout << "solid name" << endl;
-	vector<Triangle> triangles = seuillage(in, 100);
+	vector<Triangle> triangles = seuillage(in, seuil);
 	print(triangles);
 	cout << "endsolid name" << endl;
 }
 
-
+int main() {
+	//printModel("../ressources/BRAINIX/brainix.256x256x100.0.9375x0.9375x1.5.img", 200, 256, 256, 100);
+	printModel("../ressources/MANIX/manixSansIV.512x512x48.0.4570x0.4570x3.0.img", 1250, 512, 512, 48);
+	//printModel("../ressources/BEAUFIX/beaufix.448x576x72.0.6250x0.6250x1.4.img", 120, 576, 72, 448);
+	//printModel("../ressources/WHATISIT/whatisit.301x324x56.1.1.1.4.img", 50, 301, 324, 56);
+	//printModel("../ressources/engine/engine.256x256x128.1x1x1.img", 200, 256, 256, 128);
+}
